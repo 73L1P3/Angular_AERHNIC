d3fb0db6ba495fd47f107d0c38a188ea336eb7ce
73L1P3 <felipesotomayorg_98@hotmail.com>
2021-12-23 16:53:27 -0600

    Navbar created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SociosComponent } from './socios/socios.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'socios', component: SociosComponent },
+  { path: 'home', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
