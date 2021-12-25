35810eee54482094906286603d4082dece040bd9
Felipe Sotomayor <felipesotomyorg_98@hotmail.com>
2021-12-25 16:12:39 -0600

    Login Creado y agregue un par de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SociosComponent } from './socios/socios.component';
 import { AppComponent } from './app.component';
+import { NuevoComponent  } from './nuevo/nuevo.component';
+import { CumpleComponent } from './cumple/cumple.component';
+import { InicioComponent  } from './inicio/inicio.component';
+import { LoginComponent  } from './login/login.component';
 
 const routes: Routes = [
   { path: 'socios', component: SociosComponent },
-  { path: 'home', component: AppComponent }
+  { path: 'home', component: AppComponent },
+  {path: 'nuevo', component: NuevoComponent},
+  {path: 'cumple', component: CumpleComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SociosComponent } from './socios/socios.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { CumpleComponent } from './cumple/cumple.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SociosComponent
+    SociosComponent,
+    NuevoComponent,
+    CumpleComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
