c303b08c141e3342c4dca62ff2be5c04105edcdc
73L1P3 <felipesotomayorg_98@hotmail.com>
2022-09-04 17:04:32 -0600

    Agregamos el link a la Documentacion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToLink(url: string) {
+    window.open(url, '_blank');
+    //onclick="window.open(`https://drive.google.com/file/d/1pzn2jQN0PRXcG1xBKcoywyBPRv_GEjPv/view?usp=sharing`)"
   }
-
 }
